3ba5af2ccd59d26eed03d04134932f8b49e6529d
janvanrijn <janvanrijn@gmail.com>
2014-09-30 11:58:42 +0200

    updated program

--- public_html/program.php
+++ public_html/program.php
@@ -8,14 +8,14 @@
           <div class="col-md-12"><hr/></div>
         </div>
         <div class="row">
-            <div class="col-md-2">10:00-11:00</div>
+            <div class="col-md-2">12:00-13:00</div>
             <div class="col-md-3">Walk in, coffee</div>
         </div>
         <div class="row">
           <div class="col-md-12"><hr/></div>
         </div>
         <div class="row">
-            <div class="col-md-2">11:00 - 12:00</div>
+            <div class="col-md-2">13:00 - 14:00</div>
             <div class="col-md-3">Prof. Dr. Ross King (keynote)</div>
             <div class="col-md-7">Meta-Learning on QSAR data</div>
         </div>
@@ -23,14 +23,7 @@
           <div class="col-md-12"><hr/></div>
         </div>
         <div class="row">
-            <div class="col-md-2">12:00 - 13:00</div>
-            <div class="col-md-3">Lunch Break</div>
-        </div>
-        <div class="row">
-          <div class="col-md-12"><hr/></div>
-        </div>
-        <div class="row">
-            <div class="col-md-2">13:00 - 13:45</div>
+            <div class="col-md-2">14:00 - 14:45</div>
             <div class="col-md-3">Dr. Joaquin Vanschoren</div>
             <div class="col-md-7">Experiment Databases and OpenML</div>
         </div>
@@ -38,14 +31,14 @@
           <div class="col-md-12"><hr/></div>
         </div>
         <div class="row">
-            <div class="col-md-2">13:45 - 14:00</div>
+            <div class="col-md-2">14:45 - 15:00</div>
             <div class="col-md-3">Coffee break</div>
         </div>
         <div class="row">
           <div class="col-md-12"><hr/></div>
         </div>
         <div class="row">
-            <div class="col-md-2">14:00 - 14:45</div>
+            <div class="col-md-2">15:00 - 15:45</div>
             <div class="col-md-3">Prof. Dr. Luis Torgo</div>
             <div class="col-md-7">OpenML and R</div>
         </div>
@@ -53,7 +46,7 @@
           <div class="col-md-12"><hr/></div>
         </div>
         <div class="row">
-            <div class="col-md-2">14:45 - 15:30</div>
+            <div class="col-md-2">15:45 - 16:30</div>
             <div class="col-md-3">Dr. Bernd Bischl</div>
             <div class="col-md-7">R: A case study</div>
         </div>
@@ -61,14 +54,14 @@
           <div class="col-md-12"><hr/></div>
         </div>
         <div class="row">
-            <div class="col-md-2">15:30 - 16:00</div>
+            <div class="col-md-2">16:30 - 17:00</div>
             <div class="col-md-3">Coffee Break</div>
         </div>
         <div class="row">
           <div class="col-md-12"><hr/></div>
         </div>
         <div class="row">
-            <div class="col-md-2">16:00 - 16:30</div>
+            <div class="col-md-2">17:00 - 17:30</div>
             <div class="col-md-3">Jan van Rijn</div>
             <div class="col-md-7">Meta Learning on Data Streams: A case study</div>
         </div>
@@ -76,15 +69,7 @@
           <div class="col-md-12"><hr/></div>
         </div>
         <div class="row">
-            <div class="col-md-2">16:30 - 17:00</div>
-            <div class="col-md-3">TBA</div>
-            <div class="col-md-7">Interested in giving a talk? <a href="contact.php">Leave us a message!</a></div>
-        </div>
-        <div class="row">
-          <div class="col-md-12"><hr/></div>
-        </div>
-        <div class="row">
-            <div class="col-md-2">17:00 - 17:30</div>
+            <div class="col-md-2">17:30 - 18:00</div>
             <div class="col-md-3">TBA</div>
             <div class="col-md-7">Interested in giving a talk? <a href="contact.php">Leave us a message!</a></div>
         </div>
